fd08c19693a1b2faa5f69db5ac773a81013b1a9f
"Michael Alao" <"contactleomax@gmail.com">
2022-03-13 00:17:24 +0100

    fourth contract commit

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -8,6 +8,19 @@ pragma solidity >=0.7.0 <0.9.0;
  */
 contract HelloWorld {
 
+
+   struct Car { 
+        uint amout; 
+        string name; 
+    }
+
+   uint newAmount = 1000;
+
+   function createCar(uint _amount, string memory _name) external view returns (Car memory _car) {  
+        require(_amount > newAmount);
+        Car memory newCar = Car(newAmount,_name);
+        return newCar;   
+    }
     uint256 number;
 
     /**
