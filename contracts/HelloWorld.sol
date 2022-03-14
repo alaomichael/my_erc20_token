65a9c07eec71b25cb2a0a0c4624cf66732854dfb
"Michael Alao" <"contactleomax@gmail.com">
2022-03-14 14:27:35 +0100

    deployment of contract

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -2,40 +2,19 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-/**
- * @title Storage
- * @dev Store & retrieve value in a variable
- */
-contract HelloWorld {
+    contract HelloWorld{
+    string public myClientName;
+    string public tag = "Welcome ";
 
-
-   struct Car { 
-        uint amout; 
-        string name; 
+    constructor(string memory clientName){
+        myClientName = clientName;
     }
 
-   uint newAmount = 1000;
-
-   function createCar(uint _amount, string memory _name) external view returns (Car memory _car) {  
-        require(_amount > newAmount);
-        Car memory newCar = Car(newAmount,_name);
-        return newCar;   
-    }
-    uint256 number;
-
-    /**
-     * @dev Store value in variable
-     * @param num value to store
-     */
-    function store(uint256 num) public {
-        number = num;
+    function createGreeting (string memory newGreeting) public {
+        myClientName = newGreeting;
     }
 
-    /**
-     * @dev Return value 
-     * @return value of 'number'
-     */
-    function retrieve() public view returns (uint256){
-        return number;
+    function getGreeting() public view returns (string memory) {
+        return string(abi.encodePacked(tag, myClientName));
     }
 }
